d3c91f3b45d82d4fd90d8ed8c9be7b67aab16596
KanishakRajput13 <kanishakrajput08@gmail.com>
2024-10-31 02:13:59 +0530

    add textwatcher and strokes

--- app/src/main/java/com/ritika/voy/authentication/ForgotPasswordFragment.kt
+++ app/src/main/java/com/ritika/voy/authentication/ForgotPasswordFragment.kt
@@ -1,10 +1,15 @@
 package com.ritika.voy.authentication
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.ritika.voy.R
 
@@ -34,6 +39,30 @@ class ForgotPasswordFragment : Fragment() {
         val params = bottomSection.layoutParams as ConstraintLayout.LayoutParams
         params.topMargin = topMargin
         bottomSection.layoutParams = params
+
+        val emailEditText = view.findViewById<EditText>(R.id.etEmail)
+        val emailErrorTextView = view.findViewById<TextView>(R.id.tvEmailError)
+
+        emailEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val email = s.toString()
+                if (email.contains("@")) {
+                    emailEditText.background = ContextCompat.getDrawable(requireContext(), R.drawable.edit_text_background)
+                    emailErrorTextView.visibility = View.GONE
+                } else if(email.isEmpty()){
+                    emailEditText.background = ContextCompat.getDrawable(requireContext(), R.drawable.edit_text_background)
+                    emailErrorTextView.visibility = View.GONE
+                }
+                else{
+                    emailEditText.background = ContextCompat.getDrawable(requireContext(), R.drawable.edit_text_background_error)
+                    emailErrorTextView.visibility = View.VISIBLE
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
     }
 
 }
--- app/src/main/java/com/ritika/voy/authentication/OtpFragment.kt
+++ app/src/main/java/com/ritika/voy/authentication/OtpFragment.kt
@@ -2,14 +2,18 @@ package com.ritika.voy.authentication
 
 import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
 import android.text.Spannable
 import android.text.SpannableString
+import android.text.TextWatcher
 import android.text.style.ForegroundColorSpan
 import android.text.style.UnderlineSpan
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.ritika.voy.R
@@ -65,5 +69,44 @@ class OtpFragment : Fragment() {
         )
         resendTextview.text = spannable
 
+
+        val otpBox1: EditText = view.findViewById(R.id.otpBox1)
+        val otpBox2: EditText = view.findViewById(R.id.otpBox2)
+        val otpBox3: EditText = view.findViewById(R.id.otpBox3)
+        val otpBox4: EditText = view.findViewById(R.id.otpBox4)
+
+        setupOtpInput(otpBox1, otpBox2)
+        setupOtpInput(otpBox2, otpBox3)
+        setupOtpInput(otpBox3, otpBox4)
+
+        setupBackspace(otpBox2, otpBox1)
+        setupBackspace(otpBox3, otpBox2)
+        setupBackspace(otpBox4, otpBox3)
+
+    }
+    fun setupOtpInput(currentBox: EditText, nextBox: EditText) {
+        currentBox.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.toString().length == 1) {
+                    nextBox.requestFocus()
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+    }
+
+    fun setupBackspace(currentBox: EditText, previousBox: EditText) {
+        currentBox.setOnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_DEL && currentBox.text.isEmpty()) {
+                previousBox.requestFocus()
+            }
+            false
+        }
     }
 }
