05c723bc1c368167cc67ab2691470cf73b43ae48
Ritika <ritikatiwari784@gmail.com>
2024-11-14 23:07:23 +0530

    redesign bottom nav menu

--- app/src/main/java/com/ritika/voy/home/HomeActivity.kt
+++ app/src/main/java/com/ritika/voy/home/HomeActivity.kt
@@ -1,39 +1,28 @@
-// HomeActivity.kt
 package com.ritika.voy.home
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.lifecycleScope
-import com.ritika.voy.MainActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.ritika.voy.R
-import com.ritika.voy.api.DataStoreManager
-import kotlinx.coroutines.launch
+import com.ritika.voy.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        val logoutButton = findViewById<View>(R.id.logoutButton)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        logoutButton.setOnClickListener {
-            lifecycleScope.launch {
-                DataStoreManager.clearTokens(applicationContext)
-            }
-            val intent = Intent(this, MainActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(intent)
-            finish()
-        }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.bottomNavMenu.setupWithNavController(navController)
     }
 }
