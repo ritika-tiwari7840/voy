06546df553d285d2503d699eb01daf8ef4307d0b
KanishakRajput13 <kanishakrajput08@gmail.com>
2024-11-20 20:29:21 +0530

    add utility.kt

---
+++ app/src/main/java/com/ritika/voy/api/Utility.kt
@@ -0,0 +1,8 @@
+package com.ritika.voy.api
+
+class Utility {
+    companion object{
+        const val BASE_URL = "http://35.154.147.194:8000/api/auth/"
+        const val ROUTES_URL = "https://routes.googleapis.com/"
+    }
+}
