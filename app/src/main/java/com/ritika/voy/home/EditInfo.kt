55d2d88e5ec989c9c5b011925aed234de2e1780a
Ritika <ritikatiwari784@gmail.com>
2024-11-20 12:54:32 +0530

    fix back stack issue

--- app/src/main/java/com/ritika/voy/home/EditInfo.kt
+++ app/src/main/java/com/ritika/voy/home/EditInfo.kt
@@ -5,29 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.ritika.voy.R
+import com.ritika.voy.databinding.FragmentCreateAccountBinding
+import com.ritika.voy.databinding.FragmentEditInfoBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [EditInfo.newInstance] factory method to
- * create an instance of this fragment.
- */
 class EditInfo : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private var _binding: FragmentEditInfoBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -35,26 +24,19 @@ class EditInfo : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_info, container, false)
+        _binding = FragmentEditInfoBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EditInfo.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EditInfo().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = findNavController()
+        binding.btnBack.setOnClickListener {
+            navController.navigate(R.id.action_editInfo_to_profile)
+        }
     }
-}
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+    }
