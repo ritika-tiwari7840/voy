3ceff0f35474ea287d587398de3216e7b8f87521
Ritika <ritikatiwari784@gmail.com>
2024-11-26 02:04:50 +0530

    create eco layout

--- app/src/main/java/com/ritika/voy/home/EcoFragment.kt
+++ app/src/main/java/com/ritika/voy/home/EcoFragment.kt
@@ -1,60 +1,48 @@
 package com.ritika.voy.home
 
 import android.os.Bundle
+import android.provider.ContactsContract.Data
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.lifecycleScope
 import com.ritika.voy.R
+import com.ritika.voy.api.DataStoreManager
+import com.ritika.voy.databinding.FragmentChooseSpotBinding
+import com.ritika.voy.databinding.FragmentEcoBinding
+import com.ritika.voy.databinding.FragmentHomeBinding
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [EcoFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class EcoFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var _binding: FragmentEcoBinding
+    private val binding get() = _binding!!
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_eco, container, false)
+        _binding = FragmentEcoBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EcoFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EcoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launch {
+            DataStoreManager.getUserData(requireContext(), "fullName").first()!!.let {
+                val fullName = it.toString()
+                binding.userName.text = fullName
             }
+        }
     }
 }
